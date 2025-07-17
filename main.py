81cf03d421b3cd6863964d8dc2b2b6fdee3d6daf
Paul Slinkard <slinkardpaul50@gmail.com>
2025-07-17 06:44:24 -0500

    Successfully parsed and calculated the squirrel census data and shortened it for a new file.

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import pandas as pd 
 
 data = pd.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
-print(data)
+
+gray_squirrels_pop = len(data[data['Primary Fur Color'] == 'Gray'])
+cinnamon_squirrels_pop = len(data[data['Primary Fur Color'] == 'Cinnamon'])
+black_squirrels_pop = len(data[data['Primary Fur Color'] == 'Black'])
+
+census_data = {
+   'Fur Color' : ['Black', 'Grey', 'Cinnamon'],
+   'Squirrel Population' : [black_squirrels_pop, gray_squirrels_pop, cinnamon_squirrels_pop]
+}
+
+df = pd.DataFrame(census_data)
+df.to_csv('Shortened Squirrel Census data.csv')
